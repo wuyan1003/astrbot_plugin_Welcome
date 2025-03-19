650aa9783900fb6ea42fcc5cdbf184440f24ca3c
wuyan1003 <liuyantao1003@163.com>
2025-03-19 19:00:49 +0800

    astrbot_plugin_Welcome

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.api.message_components import At, Plain
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
+@register("welcome", "Astrobot", "新人入群欢迎插件", "1.0.0")
+class WelcomePlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.notice(type="group_increase")
+    async def welcome_new_member(self, event: AstrMessageEvent):
+        """新人入群时发送欢迎消息"""
+        # 从原始消息中获取新成员QQ号
+        user_id = str(event.raw_message.data["user_id"])
+        
+        # 构造欢迎消息
+        welcome_msg = [
+            At(qq=user_id),
+            Plain(" 欢迎加入本群！请仔细阅读群公告～")
+        ]
+        
+        # 发送消息
+        yield event.chain_result(welcome_msg)
 
     async def terminate(self):
-        '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
+        pass
